1bb0fb07cba8cc60d898c918d6b67c1477da36c1
Jiří Vrba <jiri@vrba.dev>
2022-05-27 15:05:41 +0200

    Whoopsie

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -16,7 +16,7 @@ const Question: FC<QuestionProps> = ({text, answers, revealed, onSelect}: Questi
                 <ul className="flex flex-col items-stretch mt-10 w-full md:w-1/2">
                     {answers.map((answer, i) => {
                         if (revealed) {
-                            return <li key={i} className={`px-10 py-4 md:py-8 m-2 rounded-xl font-black ${answer.correct ? 'text-green-800 bg-green-200' : 'text-red-800 bg-red-200'}`}>{answer.text}</li>
+                            return <li key={i} className={`px-10 py-8 m-2 rounded-xl font-black ${answer.correct ? 'text-green-800 bg-green-200' : 'text-red-800 bg-red-200'}`}>{answer.text}</li>
                         }
 
                         return <li key={i} onClick={() => onSelect(answer.correct)} className="px-10 py-8 m-2 rounded-xl font-black border transition transform hover:scale-110 hover:shadow-xl cursor-pointer bg-white">{answer.text}</li>
