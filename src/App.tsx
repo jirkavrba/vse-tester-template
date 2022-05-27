3b809b4562b36f41967972d6600576f61b7d04f6
Jiří Vrba <jiri@vrba.dev>
2022-05-27 14:49:37 +0200

    Change source set from 4st204 to 3sg201

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Tester from './components/Tester';
-import questions from "./questions.json";
+import questions from "./sets/3sg201-strategicka-analyza.json";
 
 function App() {
   return (
-    <Tester title="4ST204 - Statistika pro informatiky" questions={questions}/>
+    <Tester title="3SG201 - Strategická analýzae+" questions={questions}/>
   );
 }
 
