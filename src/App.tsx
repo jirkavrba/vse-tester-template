251d024616c1595f978b8021c38444b4c834a80d
Jiří Vrba <jiri@vrba.dev>
2022-05-27 14:17:56 +0200

    Implement storing progress in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import questions from "./questions.json";
 
 function App() {
   return (
-    // Todo: Fix this ugly (as unknown) typecast
     <Tester title="4ST204 - Statistika pro informatiky" questions={questions}/>
   );
 }
